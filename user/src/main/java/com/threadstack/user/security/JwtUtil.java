79819905064d55d13ba1fe1b68ec444aea70c9de
nylti <al.firebird@mail.com>
2025-03-12 22:44:39 -0500

    added jwt util to begin steps to issue jwt

---
+++ user/src/main/java/com/threadstack/user/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.threadstack.user.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    private final Key key;
+
+    public JwtUtil() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(String userId, String username, String role) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("username", username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
