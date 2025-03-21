d1d70a45a2ca5254deec95ff02128a4b9ad0fe29
nylti <al.firebird@mail.com>
2025-03-21 18:27:28 -0500

    updated the thread/reply logic to let moderators delete thread/reply for rooms that they moderate

--- thread/src/main/java/com/threadstack/thread/handler/ReplyHandler.java
+++ thread/src/main/java/com/threadstack/thread/handler/ReplyHandler.java
@@ -14,105 +14,119 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class ReplyHandler {
 
-    private final ReplyRepository replyRepository;
-    private final ValidationUtil validationUtil;
-    private final ThreadRepository threadRepository;
+  private final ReplyRepository replyRepository;
+  private final ValidationUtil validationUtil;
+  private final ThreadRepository threadRepository;
 
-    public ReplyHandler(ReplyRepository replyRepository, ValidationUtil validationUtil,
-	    ThreadRepository threadRepository) {
-	this.replyRepository = replyRepository;
-	this.validationUtil = validationUtil;
-	this.threadRepository = threadRepository;
+  public ReplyHandler(ReplyRepository replyRepository, ValidationUtil validationUtil,
+      ThreadRepository threadRepository) {
+    this.replyRepository = replyRepository;
+    this.validationUtil = validationUtil;
+    this.threadRepository = threadRepository;
+  }
+
+  public Mono<ServerResponse> createReply(ServerRequest request) {
+    String threadId = request.queryParam("threadId").orElse(null);
+    String parentReplyId = request.queryParam("parentReplyId").orElse(null);
+    String userName = request.headers().firstHeader("X-User-Name");
+    if (userName == null) {
+      return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
+    }
+    if (threadId == null && parentReplyId == null) {
+      return ServerResponse.badRequest()
+          .bodyValue("Either threadId or parentReply must be provided.");
+    }
+    return request.bodyToMono(Reply.class).flatMap(validationUtil::validate).flatMap(reply -> {
+      reply.setUsername(userName);
+      if (threadId != null) {
+        return threadRepository.findById(threadId)
+            .switchIfEmpty(Mono.error(new ResourceNotFoundException("Thread not found")))
+            .flatMap(thread -> {
+              reply.setThreadId(threadId);
+              return replyRepository.save(reply).flatMap(savedReply -> {
+                thread.getReplies().add(savedReply.getId());
+                return threadRepository.save(thread).thenReturn(savedReply);
+              });
+            });
+      } else {
+        return replyRepository.findById(parentReplyId)
+            .switchIfEmpty(Mono.error(new ResourceNotFoundException("Parent reply not found")))
+            .flatMap(parent -> {
+              reply.setParentReplyId(parentReplyId);
+              reply.setThreadId(parent.getThreadId());
+              return replyRepository.save(reply).flatMap(savedReply -> {
+                parent.getReplies().add(savedReply.getId());
+                return replyRepository.save(parent).thenReturn(savedReply);
+              });
+            });
+      }
+    }).flatMap(savedReply -> ServerResponse.created(URI.create("/replies/" + savedReply.getId()))
+        .contentType(MediaType.APPLICATION_JSON).bodyValue(savedReply));
+  }
+
+  public Mono<ServerResponse> getRepliesByThreadIdorParentReplyId(ServerRequest request) {
+    String threadId = request.queryParam("threadId").orElse(null);
+    String parentReplyId = request.queryParam("parentReplyId").orElse(null);
+
+    if (threadId != null) {
+      return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+          .body(replyRepository.findByThreadId(threadId), Reply.class);
+    } else if (parentReplyId != null) {
+      return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+          .body(replyRepository.findByParentReplyId(parentReplyId), Reply.class);
+    } else {
+      return Mono.empty();
+    }
+  }
+
+  public Mono<ServerResponse> getRepliesByParent(ServerRequest request) {
+    String parentReplyId = request.pathVariable("parentReplyId");
+    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+        .body(replyRepository.findByParentReplyId(parentReplyId), Reply.class);
+  }
+
+  public Mono<ServerResponse> deleteReply(ServerRequest request) {
+    String userName = request.headers().firstHeader("X-User-Name");
+    String rolesHeader = request.headers().firstHeader("X-User-Roles");
+    List<String> userRoles =
+        rolesHeader != null ? Arrays.asList(rolesHeader.split(",")) : Collections.emptyList();
+
+    String replyId = request.pathVariable("replyId");
+    if (userName == null) {
+      return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
     }
 
-    public Mono<ServerResponse> createReply(ServerRequest request) {
-	String threadId = request.queryParam("threadId").orElse(null);
-	String parentReplyId = request.queryParam("parentReplyId").orElse(null);
-	String userName = request.headers().firstHeader("X-User-Name");
-	if (userName == null) {
-	    return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
-	}
-	if (threadId == null && parentReplyId == null) {
-	    return ServerResponse.badRequest().bodyValue("Either threadId or parentReply must be provided.");
-	}
-	return request.bodyToMono(Reply.class).flatMap(validationUtil::validate).flatMap(reply -> {
-	    reply.setUsername(userName);
-	    if (threadId != null) {
-		return threadRepository.findById(threadId)
-			.switchIfEmpty(Mono.error(new ResourceNotFoundException("Thread not found")))
-			.flatMap(thread -> {
-			    reply.setThreadId(threadId);
-			    return replyRepository.save(reply).flatMap(savedReply -> {
-				thread.getReplies().add(savedReply.getId());
-				return threadRepository.save(thread).thenReturn(savedReply);
-			    });
-			});
-	    } else {
-		return replyRepository.findById(parentReplyId)
-			.switchIfEmpty(Mono.error(new ResourceNotFoundException("Parent reply not found")))
-			.flatMap(parent -> {
-			    reply.setParentReplyId(parentReplyId);
-			    reply.setThreadId(parent.getThreadId());
-			    return replyRepository.save(reply).flatMap(savedReply -> {
-				parent.getReplies().add(savedReply.getId());
-				return replyRepository.save(parent).thenReturn(savedReply);
-			    });
-			});
-	    }
-	}).flatMap(savedReply -> ServerResponse.created(URI.create("/replies/" + savedReply.getId()))
-		.contentType(MediaType.APPLICATION_JSON).bodyValue(savedReply));
+    if (replyId == null) {
+      return ServerResponse.status(HttpStatus.BAD_REQUEST)
+          .bodyValue("Missing replyId pathVariable");
     }
 
-    public Mono<ServerResponse> getRepliesByThreadIdorParentReplyId(ServerRequest request) {
-	String threadId = request.queryParam("threadId").orElse(null);
-	String parentReplyId = request.queryParam("parentReplyId").orElse(null);
-
-	if (threadId != null) {
-	    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-		    .body(replyRepository.findByThreadId(threadId), Reply.class);
-	} else if (parentReplyId != null) {
-	    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-		    .body(replyRepository.findByParentReplyId(parentReplyId), Reply.class);
-	} else {
-	    return Mono.empty();
-	}
-    }
-
-    public Mono<ServerResponse> getRepliesByParent(ServerRequest request) {
-	String parentReplyId = request.pathVariable("parentReplyId");
-	return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-		.body(replyRepository.findByParentReplyId(parentReplyId), Reply.class);
-    }
-
-    public Mono<ServerResponse> deleteReply(ServerRequest request) {
-	String userName = request.headers().firstHeader("X-User-Name");
-	String replyId = request.pathVariable("replyId");
-
-	if (userName == null) {
-	    return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
-	}
-	if (replyId == null) {
-	    return ServerResponse.status(HttpStatus.BAD_REQUEST).bodyValue("Missing replyId pathVariable");
-	}
-
-	return replyRepository.findById(replyId)
-		.switchIfEmpty(Mono.error(new ResourceNotFoundException("Reply not found"))).flatMap(reply -> {
-		    if (!userName.equals(reply.getUsername())) {
-			if (reply.getUsername() == null && "[deleted]".equalsIgnoreCase(reply.getContent())) {
-			    return ServerResponse.status(HttpStatus.BAD_REQUEST)
-				    .bodyValue("Reply has already been deleted");
-			}
-			return ServerResponse.status(HttpStatus.FORBIDDEN)
-				.bodyValue("You are not the creator of this reply");
-		    }
-		    reply.setContent("[deleted]");
-		    reply.setUsername(null);
-		    return replyRepository.save(reply).then(ServerResponse.ok().bodyValue("Reply deleted"));
-		});
-    }
-
+    return replyRepository.findById(replyId)
+        .switchIfEmpty(Mono.error(new ResourceNotFoundException("Reply not found")))
+        .flatMap(reply -> {
+          return threadRepository.findById(reply.getThreadId())
+              .switchIfEmpty(
+                  Mono.error(new ResourceNotFoundException("Thread for this reply not found")))
+              .flatMap(thread -> {
+                String roomName = thread.getRoomName();
+                boolean isModerator =
+                    userRoles.stream().anyMatch(role -> role.equalsIgnoreCase(roomName));
+                if (!userName.equals(reply.getUsername()) && !isModerator) {
+                  return ServerResponse.status(HttpStatus.FORBIDDEN)
+                      .bodyValue("You are not authorized to delete this reply.");
+                }
+                reply.setContent("[deleted]");
+                reply.setUsername(null);
+                return replyRepository.save(reply)
+                    .then(ServerResponse.ok().bodyValue("Reply deleted"));
+              });
+        });
+  }
 }
--- thread/src/main/java/com/threadstack/thread/handler/ThreadHandler.java
+++ thread/src/main/java/com/threadstack/thread/handler/ThreadHandler.java
@@ -15,65 +15,73 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ThreadHandler {
 
-    private final ThreadRepository threadRepository;
-    private final ValidationUtil validationUtil;
+  private final ThreadRepository threadRepository;
+  private final ValidationUtil validationUtil;
 
-    public Mono<ServerResponse> createThread(ServerRequest request) {
-	String userName = request.headers().firstHeader("X-User-Name");
+  public Mono<ServerResponse> createThread(ServerRequest request) {
+    String userName = request.headers().firstHeader("X-User-Name");
 
-	if (userName == null) {
-	    return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
-	}
-
-	return request.bodyToMono(Thread.class).flatMap(validationUtil::validate).map(thread -> {
-	    thread.setUserName(userName);
-	    return thread;
-	}).flatMap(threadRepository::save)
-		.flatMap(savedThread -> ServerResponse.created(URI.create("/threads/" + savedThread.getId()))
-			.contentType(MediaType.APPLICATION_JSON).bodyValue(savedThread));
+    if (userName == null) {
+      return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
     }
 
-    public Mono<ServerResponse> getThreadById(ServerRequest request) {
-	String threadId = request.pathVariable("threadId");
-	return threadRepository.findById(threadId)
-		.flatMap(thread -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(thread))
-		.switchIfEmpty(ServerResponse.notFound().build());
-    }
+    return request.bodyToMono(Thread.class).flatMap(validationUtil::validate).map(thread -> {
+      thread.setUserName(userName);
+      return thread;
+    }).flatMap(threadRepository::save).flatMap(
+        savedThread -> ServerResponse.created(URI.create("/threads/" + savedThread.getId()))
+            .contentType(MediaType.APPLICATION_JSON).bodyValue(savedThread));
+  }
 
-    public Mono<ServerResponse> getThreadsByRoom(ServerRequest request) {
-	String roomName = request.pathVariable("roomName");
-	return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-		.body(threadRepository.findByRoomName(roomName), Thread.class);
-    }
+  public Mono<ServerResponse> getThreadById(ServerRequest request) {
+    String threadId = request.pathVariable("threadId");
+    return threadRepository.findById(threadId)
+        .flatMap(
+            thread -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(thread))
+        .switchIfEmpty(ServerResponse.notFound().build());
+  }
 
-    public Mono<ServerResponse> deleteThread(ServerRequest request) {
-	String userName = request.headers().firstHeader("X-User-Name");
-	String threadId = request.pathVariable("threadId");
-	if (userName == null) {
-	    return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
-	}
-	if (threadId == null) {
-	    return ServerResponse.status(HttpStatus.BAD_REQUEST).bodyValue("Missing threadId pathVariable");
-	}
-	return threadRepository.findById(threadId)
-		.switchIfEmpty(Mono.error(new ResourceNotFoundException("Thread not found"))).flatMap(thread -> {
-		    if (!userName.equals(thread.getUserName())) {
-			if (thread.getUserName() == null && "[deleted]".equalsIgnoreCase(thread.getContent())) {
-			    return ServerResponse.status(HttpStatus.BAD_REQUEST)
-				    .bodyValue("Thread has already been deleted");
-			}
-			return ServerResponse.status(HttpStatus.FORBIDDEN)
-				.bodyValue("You are not the creator of this thread");
-		    }
-		    thread.setContent("[deleted]");
-		    thread.setUserName(null);
-		    return threadRepository.save(thread).then(ServerResponse.ok().bodyValue("Thread deleted"));
-		});
+  public Mono<ServerResponse> getThreadsByRoom(ServerRequest request) {
+    String roomName = request.pathVariable("roomName");
+    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+        .body(threadRepository.findByRoomName(roomName), Thread.class);
+  }
+
+  public Mono<ServerResponse> deleteThread(ServerRequest request) {
+    String userName = request.headers().firstHeader("X-User-Name");
+    String rolesHeader = request.headers().firstHeader("X-User-Roles");
+    List<String> userRoles =
+        rolesHeader != null ? Arrays.asList(rolesHeader.split(",")) : Collections.emptyList();
+    String threadId = request.pathVariable("threadId");
+    if (userName == null) {
+      return ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("Missing X-User-Name header");
     }
+    if (threadId == null) {
+      return ServerResponse.status(HttpStatus.BAD_REQUEST)
+          .bodyValue("Missing threadId pathVariable");
+    }
+    return threadRepository.findById(threadId)
+        .switchIfEmpty(Mono.error(new ResourceNotFoundException("Thread not found")))
+        .flatMap(thread -> {
+          boolean isModerator =
+              userRoles.stream().anyMatch(role -> role.equalsIgnoreCase(thread.getRoomName()));
+          if (!userName.equals(thread.getUserName()) && !isModerator) {
+            return ServerResponse.status(HttpStatus.FORBIDDEN)
+                .bodyValue("You are not authorized to delete this thread.");
+          }
+          thread.setContent("[deleted]");
+          thread.setUserName(null);
+          return threadRepository.save(thread)
+              .then(ServerResponse.ok().bodyValue("Thread deleted"));
+        });
+  }
 
 }
